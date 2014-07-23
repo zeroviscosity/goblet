98f104b91d74e6fe18f03acfb6a94d3d80779abe
Kent English <kent.english@gmail.com>
2014-07-22 22:47:33 -0400

    Updated gulp with nodemon and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass')
+    sass = require('gulp-sass'),
+    nodemon = require('gulp-nodemon'),
+    jshint = require('gulp-jshint'),
+    paths = {
+        angular: ['./src/js/**/*.js'],
+        node: ['./*.js', './lib/**/*.js'],
+        sass: './src/scss/*.scss',
+        css: './public/css'
+    };
+
+gulp.task('lint', function () {
+    gulp.src(paths.angular.concat(paths.node))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
 
 gulp.task('sass', function () {
-    gulp.src('src/scss/*.scss' )
+    gulp.src(paths.sass)
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest(paths.css));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+gulp.task('develop', function () {
+    nodemon({ script: 'app.js', ext: 'jade js scss' })
+        .on('change', ['lint', 'sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['lint', 'sass', 'develop']);
